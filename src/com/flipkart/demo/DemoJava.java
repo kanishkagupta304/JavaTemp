1d15cf6d9d74065d9d595075dc342f19ace854f6
Kanishka Gupta <kanishka.gupta@BLRETV-YC39MW.local>
2022-06-15 11:02:53 +0530

    hello branch commit

--- src/com/flipkart/demo/DemoJava.java
+++ src/com/flipkart/demo/DemoJava.java
@@ -3,5 +3,6 @@ package com.flipkart.demo;
 public class DemoJava {
     public static void main(String args[]){
         System.out.println("Testing...");
+        hi
     }
 }
---
+++ src/com/flipkart/demo/HelloBranchClass.java
@@ -0,0 +1,7 @@
+package com.flipkart.demo;
+
+public class HelloBranchClass {
+    public static void main(String[] args){
+        System.out.println("new hello branch");
+    }
+}
